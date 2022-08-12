3e2fe20ea3bc27053ae2c76b83ec128cd206c3ed
PavelRoR <pavelvp2182@yandex.ru>
2022-08-12 17:32:33 +0500

    Main page is typeseted

--- js/scripts.js
+++ js/scripts.js
@@ -27,69 +27,69 @@ $(document).ready(function () {
 // модалка
 $('.button-up').fancybox();
 // слайдер с видео-отзывами
-// $('.revs-slider').slick({
-//     autoplay: false,
-//     autoplaySpeed: 3000,
-//     slidesToShow: 3,
-//     slidesToScroll: 1,
-//     infinite: true,
-//     dots: true,
-//     speed: 300,
-//     arrows: true,
-//     centerPadding: '40px',
-//     // adaptiveHeight: true,
-//     centerMode: true,
-//     appendArrows: '.video-revs-arrows-1',
-//     responsive: [{
-//         breakpoint: 768,
-//         settings: {
-//             slidesToShow: 1
-//         }
-//     }]
-// });
+$('.revs-slider').slick({
+    autoplay: false,
+    autoplaySpeed: 3000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    infinite: true,
+    dots: true,
+    speed: 300,
+    arrows: true,
+    centerPadding: '40px',
+    // adaptiveHeight: true,
+    centerMode: true,
+    appendArrows: '.video-revs-arrows-1',
+    responsive: [{
+        breakpoint: 768,
+        settings: {
+            slidesToShow: 1
+        }
+    }]
+});
 // слайдер с текстовыми отзывами
-// $('.revs-slider-text').slick({
-//     autoplay: false,
-//     autoplaySpeed: 3000,
-//     slidesToShow: 3,
-//     slidesToScroll: 1,
-//     infinite: true,
-//     dots: true,
-//     speed: 300,
-//     arrows: true,
-//     centerPadding: '40px',
-//     adaptiveHeight: true,
-//     centerMode: true,
-//     appendArrows: '.video-revs-arrows-2',
-//     responsive: [{
-//         breakpoint: 768,
-//         settings: {
-//             slidesToShow: 1
-//         }
-//     }]
-// });
-// $('.slick-dots').wrap('<div class="container container-revs">')
+$('.revs-slider-text').slick({
+    autoplay: false,
+    autoplaySpeed: 3000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    infinite: true,
+    dots: true,
+    speed: 300,
+    arrows: true,
+    centerPadding: '40px',
+    adaptiveHeight: true,
+    centerMode: true,
+    appendArrows: '.video-revs-arrows-2',
+    responsive: [{
+        breakpoint: 768,
+        settings: {
+            slidesToShow: 1
+        }
+    }]
+});
+$('.slick-dots').wrap('<div class="container container-revs">')
 
-// function imToVideo() {
-//     $('.video-wrapper-revs iframe').each(function () {
-//         var l = $(this).parent().attr('data-img');
-//         $(this).parent().html('<img src="' + l + '" alt="Видео отзыв"><div class="yt-button"></div>');
-//     });
-//     $(".video-wrapper-revs img, .yt-button").click(function () {
-//         var a = $(this).parent().attr("data-youtube");
-//         $(this).parent().html('<iframe src="https://www.youtube.com/embed/' + a + '?showinfo=0&rel=0&autoplay=1&mute=1&modestbranding=1" allowfullscreen></iframe>')
-//     });
-// }
-// $('.revs-slider').on('swipe', function (event, slick, direction) {
-//     imToVideo();
-// });
-// $('.revs-slider').on('afterChange', function (event, slick, direction) {
-//     imToVideo();
-// });
-// $('.revs-slider').on('beforeChange', function (event, slick, direction) {
-//     imToVideo();
-// });
-// /* Видео */
-// $(".video-wrapper-revs img, .yt-button").click(imToVideo());
+function imToVideo() {
+    $('.video-wrapper-revs iframe').each(function () {
+        var l = $(this).parent().attr('data-img');
+        $(this).parent().html('<img src="' + l + '" alt="Видео отзыв"><div class="play-button"></div>');
+    });
+    $(".video-wrapper-revs img, .play-button").click(function () {
+        var a = $(this).parent().attr("data-youtube");
+        $(this).parent().html('<iframe src="https://www.youtube.com/embed/' + a + '?showinfo=0&rel=0&autoplay=1&mute=1&modestbranding=1" allowfullscreen></iframe>')
+    });
+}
+$('.revs-slider').on('swipe', function (event, slick, direction) {
+    imToVideo();
+});
+$('.revs-slider').on('afterChange', function (event, slick, direction) {
+    imToVideo();
+});
+$('.revs-slider').on('beforeChange', function (event, slick, direction) {
+    imToVideo();
+});
+/* Видео */
+$(".video-wrapper-revs img, .play-button").click(imToVideo());
 /*Конец документа*/
 });
